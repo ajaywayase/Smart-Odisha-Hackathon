37d1ccf91aff56bd273465d6f36d4a2c2004c2da
Suraj Vyavhare <35147620+risingstar8030@users.noreply.github.com>
2018-11-21 22:02:47 +0530

    Update server.js

--- src/app/serverside/server.js
+++ src/app/serverside/server.js
@@ -5,24 +5,6 @@ let mongoose = require('mongoose');
 let cors = require('cors');
 let app = express();
 
-/*const twilio = require('twilio')('AC71b131cf19d866871889af07e97ca213','b51891b5ece92bb1718f4b38a3862c9a');
-
-const numbers = [
-   '+918208997016','+918830311899'
-]
-Promise.all(
-    numbers.map(number => {
-      return twilio.messages.create({
-        to: number,
-        from:'+12172866559' ,
-        body: "Last Trial"
-      });
-    })
-  ) .then(messages => {
-      console.log('Messages sent!');
-    })
-    .catch(err => console.error(err));*/
-
 let signupRoutes = require('./route/signup');
 let signinRoutes = require('./route/signin');
 let fcrRoutes = require('./route/fcr');
